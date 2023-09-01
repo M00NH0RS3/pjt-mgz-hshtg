d334c8919bc5df219d3a52ec17aada183c6ef5bb
M00NH0RS3 <matheusmaciel.trampas@gmail.com>
2023-09-01 10:26:59 -0300

    j

--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -1,5 +1,7 @@
 import { catalogo } from "./utilidades";
 
+const idsProdutoCarrinhoComQuantidade = {}
+
 function abrirCarrinho() {
     document.getElementById("carrinho").classList.remove("right-[-360px]");
     document.getElementById("carrinho").classList.add("right-[0px]");
@@ -18,31 +20,100 @@ export function inicializarCarrinho() {
     botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
     }
 
-export function adcionarAoCarrinho(idProduto) {
+function removerDoCarrinho(idProduto){
+    delete idsProdutoCarrinhoComQuantidade[idProduto];
+    renderizarProdutosCarrinho();
+    }
+
+function incrementarQuantidadeProduto(idProduto){
+    idsProdutoCarrinhoComQuantidade[idProduto]++;
+    atualizarInformacaoQuantidade(idProduto);
+    }
+
+function decrementarQuantidadeProduto(idProduto){
+    if(idsProdutoCarrinhoComQuantidade[idProduto] === 1){
+        removerDoCarrinho(idProduto);
+        return;
+    }
+    idsProdutoCarrinhoComQuantidade[idProduto]--;
+    atualizarInformacaoQuantidade(idProduto);
+    }
+
+function atualizarInformacaoQuantidade(idProduto){
+    document.getElementById(`quantidade-${idProduto}`).innerText = 
+    idsProdutoCarrinhoComQuantidade[idProduto];
+}
+
+function desenharProdutoNoCarrinho(idProduto){
     const produto = catalogo.find((p) => p.id === idProduto)
     const containerProdutoCarrinho = document.getElementById("produtos-carrinho");
-    const cartaoProdutoCarrinho = `
-        <article class="flex bg-slate-100 rounded-lg pg-1 relative">
-            <button id="fechar-carrinho" class="absolute top-0 right-2">
-                <i class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-800"></i>
-            </button>
-            <img
-                src="./assets/img/${produto.imagem}"
-                alt="Carrinho: ${produto.nome}"
-                class="h-24 rounded-lg p-1"
-            />
-            <div class="py-2">
-                <p class="text-slate-900 text-sm">
-                    ${produto.nome}
-                </p>
-                <p class="text-slate-400 text-xs">
-                    Tamanho: M
-                </p>
-                <p class="text-green-700 text-lg">
-                    $${produto.preco},00
-                </p>
-            </div>
-        </article>
-    `;
-    containerProdutoCarrinho.innerHTML += cartaoProdutoCarrinho;
+    
+    const elementoArticle = document.createElement('article');
+    const articleClasses = [
+        'flex',
+        'bg-slate-100',
+        'rounded-lg',
+        'pg-1',
+        'relative'
+    ];
+
+    for (const articleClass of articleClasses) {
+        elementoArticle.classList.add(articleClass);
+    }
+
+    const cartaoProdutoCarrinho = `<button id="remover-item-${produto.id}" class="absolute top-0 right-2">
+            <i class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-800"></i>
+        </button>
+        <img
+            src="./assets/img/${produto.imagem}"
+            alt="Carrinho: ${produto.nome}"
+            class="h-24 rounded-lg p-1"
+        />
+        <div class="p-2 flex flex-col justify-between">
+            <p class="text-slate-900 text-sm">
+                ${produto.nome}
+            </p>
+            <p class="text-slate-400 text-xs">
+                Tamanho: M
+            </p>
+            <p class="text-green-700 text-lg">
+                $${produto.preco},00
+            </p>
+        </div>
+        <div class='flex text-slate-950 items-end absolute bottom-0 right-2 text-lg'>
+            <button id='decrementar-produto-${produto.id}'>-</button>
+            <p id='quantidade-${produto.id}' class='ml-2'>${idsProdutoCarrinhoComQuantidade[produto.id]}</p>
+            <button id='incrementar-produto-${produto.id}' class='ml-2'>+</button>
+        </div>`;
+    
+    elementoArticle.innerHTML = cartaoProdutoCarrinho;
+    containerProdutoCarrinho.appendChild(elementoArticle);
+
+    document.getElementById(`incrementar-produto-${produto.id}`).addEventListener('click', () => 
+    incrementarQuantidadeProduto(produto.id));
+    
+    document.getElementById(`decrementar-produto-${produto.id}`).addEventListener('click', () => 
+    decrementarQuantidadeProduto(produto.id));
+    
+    document.getElementById(`remover-item-${produto.id}`).addEventListener('click', () => 
+    removerDoCarrinho(produto.id));
+}
+
+function renderizarProdutosCarrinho(){
+    const containerProdutoCarrinho = document.getElementById('produtos-carrinho');
+    containerProdutoCarrinho.innerHTML = '';
+    
+    for(const idProduto	in idsProdutoCarrinhoComQuantidade){
+        desenharProdutoNoCarrinho(idProduto);
+    }
+}
+
+
+export function adcionarAoCarrinho(idProduto) {
+    if (idProduto in idsProdutoCarrinhoComQuantidade){
+        incrementarQuantidadeProduto(idProduto);
+        return;
+    }
+    idsProdutoCarrinhoComQuantidade[idProduto] = 1;
+    desenharProdutoNoCarrinho(idProduto);
     }
