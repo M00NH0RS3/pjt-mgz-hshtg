b9d4203101a7e8935375d83b7327300ef7ac7b50
M00NH0RS3 <matheusmaciel.trampas@gmail.com>
2023-09-04 10:43:07 -0300

    RF

--- checkout.js
+++ checkout.js
@@ -1,11 +1,40 @@
-import { desenharProdutoNoCarrinhoSimples, lerLocalStorage } from "./src/utilidades";
+    import {
+    desenharProdutoNoCarrinhoSimples,
+    lerLocalStorage, apagarDoLocalStorage, salvarLocalStorage
+    } from "./src/utilidades";
 
-
-function desenharProdutosCheckout(){
-    const idsProdutoCarrinhoComQuantidade = lerLocalStorage('carrinho');
-    for(const idProduto in idsProdutoCarrinhoComQuantidade){
-        desenharProdutoNoCarrinhoSimples(idProduto, 'container-produtos-checkout', idsProdutoCarrinhoComQuantidade[idProduto]);
+    function desenharProdutosCheckout() {
+        const idsProdutoCarrinhoComQuantidade = lerLocalStorage("carrinho") ?? {};
+        for (const idProduto in idsProdutoCarrinhoComQuantidade) {
+            desenharProdutoNoCarrinhoSimples(
+            idProduto,
+            "container-produtos-checkout",
+            idsProdutoCarrinhoComQuantidade[idProduto]
+            );
+        }
     }
-}
 
-desenharProdutosCheckout();
+    function finalizarCompra(evento){
+        evento.preventDefault();
+        const idsProdutoCarrinhoComQuantidade = lerLocalStorage("carrinho") ?? {};
+        if (Object.keys(idsProdutoCarrinhoComQuantidade).length === 0){
+            return;
+        }
+        const dataAtual = new Date();
+        const pedidoFeito = {
+            dataPedido: dataAtual,
+            pedido: idsProdutoCarrinhoComQuantidade
+        }
+        
+        const historicoDePedidos = lerLocalStorage('historico') ?? [];
+        const historicoDePedidosAtualizado = [pedidoFeito, ...historicoDePedidos];
+
+        salvarLocalStorage('historico', historicoDePedidosAtualizado)
+        apagarDoLocalStorage('carrinho')
+        
+        window.location.href = window.location.origin + '/pedidos.html';
+    }
+
+    desenharProdutosCheckout();
+
+    document.addEventListener('submit', (evt) => finalizarCompra(evt));
